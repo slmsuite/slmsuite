ff56a1dc1c7c96c0f67a3d7abe95ed7c8bf9a998
Ian Christen <ichr@mit.edu>
2024-11-22 20:34:18 -0500

    Increment release.

--- slmsuite/holography/algorithms/_spots.py
+++ slmsuite/holography/algorithms/_spots.py
@@ -99,7 +99,7 @@ class CompressedSpotHologram(_AbstractSpotHologram):
             More general or complicated functionality requires full use of the GPU
             and thus the following option requires :mod:`cupy` and underlying CUDA.
 
-        #.  **(This feature is currently disabled until 0.1.1 due
+        #.  **(This feature is currently disabled until 0.1.3 due
             to discovered instability on different systems.)**
             A custom CUDA kernel loaded into :mod:`cupy`.
             Above a certain number of spots (:math:`O(10^3)`),

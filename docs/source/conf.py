a4a953a6af1e1e232a3effc5fce7f942bd335183
Ian Christen <ichr@mit.edu>
2024-11-30 22:09:02 -0500

    Increment version to 0.2.0.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -32,7 +32,7 @@ for module_path in module_paths:
 project = "slmsuite"
 copyright = "2024, slmsuite Developers"
 author = "slmsuite Developers"
-release = "0.1.2"
+release = "0.2.0"
 
 # -- General configuration ---------------------------------------------------
 
